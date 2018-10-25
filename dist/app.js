5ef4ce69cdc84612c0d0994eeb3cd694a84fbd37
Bruno Vieira Campos <brunovieiracampos@mbp-de-bruno.home>
2018-10-24 23:05:24 -0300

    adicionando classe app.ts

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,12 @@
-console.log("taaa");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+    }
+    middleware() {
+        this.express.use('/hello', (req, res, next) => {
+        });
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,17 @@
-console.log("taaa");
+import * as express from 'express';
+
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+    }
+
+    private middleware(): void {
+        this.express.use('/hello', (req, res, next) => {
+
+        });
+    }
+
+
+}
