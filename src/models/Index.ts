ea18a306f79f1a90f6168e97ee05ca601a93e139
Bruno Campos <bruno.campos@cbyk.com.br>
2018-11-05 12:15:26 -0200

    ajustes nome arquivo index do models

--- src/models/Index.ts
+++ src/models/Index.ts
@@ -2,32 +2,22 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as Sequelize from 'sequelize';
 
-import { DbConnection } from './../interfaces/DbConnectionInterface';
+import { DbConnection } from '../interfaces/DbConnectionInterface';
 
-// Para ver os console.logs desse arquivo, rode o comando node dist\models\index.js
-
-// ------------------------------------------
 const basename: string = path.basename(module.filename);
-
-// ------------------------------------------
-
-
-// Pegando o ambiente
-const env: string = process.env.NODE_ENV.trim() || 'development';
-
-// Encontrando o arquivo de configuração do Sequelize
+const env: string = process.env.NODE_ENV || 'development';
 let config = require(path.resolve(`${__dirname}./../config/config.json`))[env];
-
-
-// ------------------------------------------
-
-
 let db = null;
 
-// Caso não exista uma conexão, criamos uma
 if (!db) {
 
     db = {};
+
+    const operatorsAliases = {
+        $in: Sequelize.Op.in
+    };
+
+    config = Object.assign({operatorsAliases}, config);
 
     const sequelize: Sequelize.Sequelize = new Sequelize(
         config.database,
@@ -37,32 +27,24 @@ if (!db) {
     );
 
     fs
-      .readdirSync(__dirname)
-      // Mapeando as models e filtrando para não mapear o index.js junto
-      .filter((file: string) => {
-        return file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js";
-      })
-      // Este bloco de código importa todas as models para dentro da variável db
-      .forEach((file: string) => {
-        const model = sequelize.import(path.join(__dirname, file));
-        db[model["name"]] = model;
-      });
+        .readdirSync(__dirname)
+        .filter((file: string) => {
+            const fileSlice: string = file.slice(-3);
+            return (file.indexOf('.') !== 0) && (file !== basename) && ((fileSlice === '.js') || (fileSlice === '.ts'));
+        })
+        .forEach((file: string) => {
+            const model = sequelize.import(path.join(__dirname, file));
+            db[model['name']] = model;
+        });
 
-    // Percorremos as models da nossa db e verificamos se a Model tem o método associate
     Object.keys(db).forEach((modelName: string) => {
         if (db[modelName].associate) {
-            // Caso tenha, passa os módulos que temos
             db[modelName].associate(db);
         }
     });
 
-    // Serve para sincronizar o Sequelize com o MySQL
-    db['sequelize'] = sequelize;    
+    db['sequelize'] = sequelize;
 
 }
-
-
-// ------------------------------------------
-
 
 export default <DbConnection>db;
